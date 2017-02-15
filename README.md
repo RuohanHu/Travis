91d856cfe5f2ec671fec331b708e55dd4cf3b5f2
Ruohan Hu <Steven@Ruohans-MacBook-Pro-2.local>
2017-02-15 11:54:47 -0800

    hello

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Build
+Status](https://travis-ci.org/RuohanHu/gittest.svg?branch=master)](https://travis-ci.org/RuohanHu/gittest)
---
+++ src/edu/ucsd/cs110/temperature/Temperature.java
@@ -0,0 +1,20 @@
+package edu.ucsd.cs110.temperature;
+
+/**
+ * Created by Steven on 2/15/17.
+ */
+public abstract class Temperature {
+    private float value;
+
+    public Temperature(float v)
+        {
+            value = v;
+        }
+        public final float getValue()
+        {
+            return value;
+        }
+        public abstract Temperature toCelsius();
+        public abstract Temperature toFahrenheit();
+}
+
